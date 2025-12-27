311507cbffbd70f1f035be3f6b54cd5291b0c1e0
fedevita <43930218+fedevita@users.noreply.github.com>
2025-12-27 22:09:24 +0100

    broken test

---
+++ tests/TheTimeLedger.SharedKernel.Tests/Primitives/BreakTest.cs
@@ -0,0 +1,11 @@
+namespace TheTimeLedger.SharedKernel.Tests.Primitives;
+
+public class BreakTest
+{
+    [Fact]
+    public void This_test_is_intentionally_broken()
+    {
+        // Questo test DEVE fallire.
+        Assert.True(false);
+    }
+}
